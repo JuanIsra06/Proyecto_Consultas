d3e5304c6342092411742ed5f39cc8d5b43705bd
JuanIsra06 <celedonjuan06@gmail.com>
2022-09-14 18:24:04 -0500

    Final commit

---
+++ xs.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <conio.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int x;
+
+int main()
+{
+	cout<<"\nEl numero que ingreso se repite "<<cont<<" veces";
+	cin>>x;
+	
+	if(x=1)
+	{
+		cout<<"ALV PERRO HACKEASTE LA NASA EQUISDE ERES LA VERGA jsjsjsj k kreisi SKELEEEEEEEEEEEER";
+	}
+	else
+	{
+		cout<<"TAS BIEN PENDEJO NO SABES HACKEAR EQUISDE";
+	}
+	getch();
+}
